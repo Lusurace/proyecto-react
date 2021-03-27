ec8ed9b539fdd6f9537953fb3b82036515cde11d
Luciana Surace <lusurace@gmail.com>
2021-03-27 13:29:32 -0300

    idea+apellido

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+      <nav class="navbar navbar-light bg-light"/>
+          <h2>Las ofertas de la semana</h2>
+     </>;
+}
+
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+<div class="fixed-top">
+			  	<div class="collapse" id="navbarToggleExternalContent">
+					<div class="bg-light p-4">
+						<h5 class="text-white h4">LupeDeco</h5>
+						<ul class="text-muted">
+							<li class="nav-item">
+								<a class="nav-link active" href="index.html">Inicio</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="producto.html">Productos</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="blog.html">Blog</a>
+								<ul>
+									<li class="nav-item"> <a href="blog.html#recetas">Kraft</a></li>
+									<li class="nav-item" > <a href="blog.html#planeta">Planeta</a></li>
+								</ul>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="nosotros.html">Nosotros</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="contacto.html">Contacto</a>
+							</li>
+						</ul>
+					</div>
+			  </div>
+			  <nav class="navbar navbar-light bg-light">
+			    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+			      <span class="navbar-toggler-icon"></span>
+			    </button>
+			  </nav>
+			</div>
